2e243c98b91ae05fe656ea478add4407d491ac4b
Mido <43594864+midoandev@users.noreply.github.com>
2021-12-06 16:31:16 +0700

    add skill and my work

--- src/Helper.js
+++ src/Helper.js
@@ -3,6 +3,22 @@ import * as React from "react"
 import I18n from 'react-native-i18n'
 // I18n.locale = 'en'
 
+export function Umurku() { 
+    let brth = new Date(1995,8,11);
+    let ageDifMs = Date.now() - brth.getTime();
+    let ageDate = new Date(ageDifMs); // miliseconds from epoch
+    let hasil = Math.abs(ageDate.getUTCFullYear() - 1970); 
+    return hasil
+}
+
+export function WaktuKerja() { 
+    let brth = new Date(2018,9,11);
+    let ageDifMs = Date.now() - brth.getTime();
+    let ageDate = new Date(ageDifMs); // miliseconds from epoch
+    let hasil = Math.abs(ageDate.getUTCFullYear() - 1970); 
+    return hasil
+}
+
 export const tr = ( ) => { 
     I18n.fallbacks = true; 
     I18n.defaultLocale = 'en'
@@ -11,8 +27,11 @@ export const tr = ( ) => {
             "title1": "React Naitve (Android & IOS) Developer",
             "title2": "Write code, keep learning, and solve the problem", 
             "name": "Hi, I'm Mido. Nice to meet you.",
-            "desc": "I started my career as a mobile developer since 3 years ago at Yogyakarta. Starting from the Java programming language, making  apps using the Android Studio IDE, to switching to JavaScript, created cross-platform Android and IOS applications using the React Native framework. I'm perfectionis in positive and always learning to improve skills in developing mobile applications.",
+            "desc": "I started my career as a mobile developer since "+WaktuKerja()+" years ago at Yogyakarta. Starting from the Java programming language, making  apps using the Android Studio IDE, to switching to JavaScript, created cross-platform Android and IOS applications using the React Native framework. I'm perfectionis in positive and always learning to improve skills in developing mobile applications.",
+            "about": " About me, My full name is Agus Rakhmadi Mido, I was born in Tarakan "+Umurku()+" years ago. I received a bachelor's degree in computer science in 2018 and a master's degree in informatics technology in 2021 at the Yogyakarta Technological University. I'm a quiet person but I easily socialize, meet, discuss, and even work with people I just met. I'm also known as a humble person and good listener so that I understand the essense of the problem and provide the right solution.",
+            "exp": "I worked at Imkom Academy as a Programmer from september 2018 until now. Imkom Academy is the place for courses in technology. I also work as a teacher in the Java programming language, especially Android-based mobile apps.",
             "Portofolio": 'Portofolio',
+            "work": 'Work',
             "iosstore": 'Available on the app store',
             "playstore": 'Available on the play store',
             "skills": 'Skills',
@@ -23,8 +42,11 @@ export const tr = ( ) => {
             "title1": "React Naitve (Android & IOS) Developer",
             "title2": "Menulis kode, tetap belajar, dan menyelesaikan masalah",
             "name": "Hi, saya Mido. Senang berkenalan dengan Anda",
-            "desc": "Saya memulai karir sebagai pengembang aplikasi mobile sejak 3 tahun yang lalu di Yogyakarta. Mulai dari bahasa pemrograman Java, membuat aplikasi menggunakan Android Studio IDE, hingga beralih ke JavaScript, membuat aplikasi lintas platform Android dan iOS menggunakan framework React Native. Saya perfeksionis dalam hal positif dan selalu belajar untuk meningkatkan keterampilan dalam mengembangkan aplikasi mobile." ,
+            "desc": "Saya memulai karir sebagai pengembang aplikasi mobile sejak "+WaktuKerja()+" tahun yang lalu di Yogyakarta. Mulai dari bahasa pemrograman Java, membuat aplikasi menggunakan Android Studio IDE, hingga beralih ke JavaScript, membuat aplikasi lintas platform Android dan iOS menggunakan framework React Native. Saya perfeksionis dalam hal positif dan selalu belajar untuk meningkatkan keterampilan dalam mengembangkan aplikasi mobile." ,
+            "about": "Sedikit tentang saya, Nama lengkap saya Agus Rakhmadi Mido, saya lahir di Tarakan "+Umurku()+" tahun yang lalu. Saya memperoleh gelar sarjana di bidang teknik komputer pada tahun 2018 dan gelar master teknologi informatika pada tahun 2021 di Universitas Teknologi Yogyakarta. Saya orangnya pendiam tapi saya mudah bersosialisasi, bertemu, berdiskusi, bahkan bekerja dengan orang yang baru saya kenal. Saya juga dikenal rendah hati dan pendengar yang baik sehingga saya memahami inti permasalahan dan memberikan solusi yang tepat.",
+            "exp": "Saya bekerja di Imkom Academy sebagai Programmer dari September 2018 sampai sekarang, Imkom Academy adalah tempat kursus di bidang teknologi. Saya juga berprofesi sebagai pengajar di bidang bahasa pemrograman Java khususnya mobile apps berbasis Android.",
             "Portofolio": 'Portofolio',
+            "work": 'Kerja',
             "iosstore": 'Tersedia di AppStore',
             "playstore": 'Tersedia di Playstore',
             "skills": 'Keterampilan',
--- src/LearnSomething.js
+++ src/LearnSomething.js
@@ -2,11 +2,13 @@ import { t } from 'i18n-js';
 import React, { Component } from 'react';
 import { View, Text, Dimensions, Image, ScrollView, TouchableOpacity, StyleSheet, FlatList, Linking } from 'react-native'; 
 import Css from './Css';
-import {tr} from "./Helper";
-import { IconMe, IconPortopolio, IconSkilss, Logoarm, MobileExpletion } from './SVGHelp';
+import {agees, tr, Umurku} from "./Helper";
+import { IconExperience, IconMe, IconPortopolio, IconSkilss, Logoarm, MobileExpletion } from './SVGHelp';
 const {width, height} = Dimensions.get('window')
 import { Feather, FontAwesome5, Ionicons, Fontisto } from "@expo/vector-icons";
 
+console.log('dimens', JSON.stringify(Dimensions.get('window')))
+const inMobile = width < 500
 
 class LearnSomething extends Component {
   constructor(props) {
@@ -34,21 +36,30 @@ class LearnSomething extends Component {
           {name: 'Droppoint Paketindo', desc: '', path: require('../assets/dp.png'), and: 'https://play.google.com/store/apps/details?id=com.pektindokurirdroppoint', ios: '',
            doc: 'Aplikasi memiliki fitur yang sederhana seperti mengisikan formulir untuk bertransaksi'},
           {name: 'Pasarindo', desc: '', path: require('../assets/pasar.png'), and: 'https://play.google.com/store/apps/details?id=com.pasarindoapp', ios: '',
-          doc: 'Aplikasi E-commerce yang berpusat pada Pasar lokal, memiliki fitur belanja dan berjualan. seperti aplikasi E-commerce lainnya.'},
+            doc: 'Aplikasi E-commerce yang berpusat pada Pasar lokal, memiliki fitur belanja dan berjualan. seperti aplikasi E-commerce lainnya.'},
           {name: 'PesananQu', desc: '', path: require('../assets/pesananqu.png'), and: '', ios: '',
-          doc: 'Aplikasi E-commerce yang berpusat pada buah tangan atau biasa disebut oleh-oleh, memiliki fitur belanja dan berjualan. seperti aplikasi E-commerce lainnya.'},
+            doc: 'Aplikasi E-commerce yang berpusat pada buah tangan atau biasa disebut oleh-oleh, memiliki fitur belanja dan berjualan. seperti aplikasi E-commerce lainnya.'},
         ],
         skills: [
           {title: 'React Native', sub: 'React'},
           {title: 'React Redux', sub: 'React'},
+          {title: 'React Hook', sub: 'React'},
+          {title: 'Expo Web', sub: 'React'},
           {title: 'Firebase Realtime Database', sub: 'React'},
           {title: 'Firebase Cloud Message', sub: 'React'},
           {title: 'Google Map', sub: 'React'}, 
           {title: 'Google Map Autocomplete', sub: 'React'},
           {title: 'Google Map Direction', sub: 'React'},
           {title: 'Google Map Place', sub: 'React'},
-          {title: 'Android Studio (in Java)', sub: 'React'},
-          {title: 'C++', sub: 'React'},
+          {title: 'Java', sub: 'React'},
+          {title: 'Experience with Android Java and iOS Objective-C', sub: 'React'},
+          {title: 'Familiarity with REST and JSON', sub: 'React'},
+          {title: 'Experience with third-party libraries and APIs', sub: 'React'},
+          {title: 'Strong logical', sub: 'React'},
+          {title: 'Good Communication', sub: 'React'},
+          {title: 'Teamwork', sub: 'React'},
+          {title: 'English (Passive)', sub: 'React'},
+          {title: 'C++ (Beginner)', sub: 'React'},
           {title: 'Git', sub: 'React'},
         ]
 
@@ -58,12 +69,12 @@ class LearnSomething extends Component {
     let st = this.state
     return (
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{backgroundColor: '#eee'}} ref={ref => this.listViewRef = ref}>
-        <View style={{flex:1, minHeight: 600}}>
-          <View style={{ width,height:600, flex:1, position:'absolute', justifyContent: 'center', alignItems: 'center', opacity:.1}}>
+        <View style={{flex:1, minHeight: height}}>
+          <View style={{ width,height:height, flex:1, position:'absolute', justifyContent: 'center', alignItems: 'center', opacity:.1}}>
             <MobileExpletion />
           </View>
           
-          <View style={{flexDirection: 'row', justifyContent:'space-between', padding:24, marginTop:24, alignItems: 'center', flex:1}}>
+          <View style={{flexDirection: 'row', justifyContent:'space-between', paddingHorizontal:24, marginTop:16, alignItems: 'center', flex:1}}>
               <Logoarm/>
               <View style={{ flexDirection: 'row', justifyContent:'center',   }}> 
           
@@ -78,10 +89,11 @@ class LearnSomething extends Component {
                   
                   <TouchableOpacity disabled={st.tab==1}
                     onPress={() => {
-                      // tr().locale = 'id'
-                      // this.setState({tab: 1}) 
+                      tr().locale = 'id'
+                      this.setState({tab: 1}) 
                       
-                      this.listViewRef.scrollTo({y: 10, animated: true});
+                      // this.listViewRef.scrollTo({y: 10, animated: true});
+                      // console.log('dsadas', Umurku())
                     }}
                   activeOpacity={.8} style={st.tab==1 ? Css.textActive : Css.textUnActive}>
                   <Text style={{color:'#000', fontWeight:'500', opacity:st.tab==1?1:.6}}>ID</Text>
@@ -97,48 +109,50 @@ class LearnSomething extends Component {
             </View>
             <Image
               resizeMode={'contain'}
-              style={[Css.LayoutShadow, {width: 190,height: 190, marginBottom:60, backgroundColor: 'transparent' }]}
+              style={[Css.LayoutShadow, {width: 190,height: 190, backgroundColor: 'transparent' }]}
               source={require('../assets/meraj.png')}
             />
           </View>
         </View>
-        <View style={{backgroundColor: '#B42929', paddingVertical:80, alignItems: 'center', justifyContent:'center', paddingHorizontal: 40 }}>
+        <View style={{backgroundColor: '#B42929', paddingBottom:80, paddingTop:30, alignItems: 'center', justifyContent:'center', paddingHorizontal: 40, marginBottom:80 }}>
           <IconMe/>
-          <Text style={{fontWeight: '500', fontSize: 24, lineHeight:32, letterSpacing:1, marginBottom:24, textAlign: 'center', color: '#fff'}}>{tr().t('name')}</Text>
-          <Text style={{fontWeight: '400', fontSize: 18, lineHeight:30, textAlign: 'center', color: '#fff', paddingHorizontal:60}}>{tr().t('desc')}</Text>
+          <Text style={{fontWeight: '500', fontSize: 24, lineHeight:32, letterSpacing:1, marginVertical:28, textAlign: 'center', color: '#fff'}}>{tr().t('name')}</Text>
+          <Text style={{fontWeight: '400', fontSize: 18, lineHeight:30, textAlign: 'center', color: '#fff',marginBottom:28, paddingHorizontal:60}}>{tr().t('desc')}</Text>
+          <Text style={{fontWeight: '400', fontSize: 18, lineHeight:30, textAlign: 'center', color: '#fff', paddingHorizontal:60, marginBottom:38,}}>{tr().t('about')}</Text>
+
+          <View style={{paddingVertical: 20, borderRadius: 12, borderColor: '#fff', borderWidth:1, width: inMobile ? width : width*.6,
+            flexDirection: inMobile ?'column': 'row', marginHorizontal: 40, alignItems:"center"}}>
+            <IconExperience/>
+            <View style={{paddingHorizontal:24, flex:1}}>
+            <Text style={{fontWeight: '600', fontSize: 16, lineHeight:25, textAlign: inMobile? 'center':'left', color: '#fff',  }}>{tr().t('work')}</Text>
+              <Text style={{fontWeight: '400', fontSize: 13, lineHeight:25, textAlign: inMobile? 'center':'left', color: '#fff',  }}>{tr().t('exp')}</Text>
+            </View>
+          </View>
         </View>
 
+         
         
-        <View style={{flex:1, alignItems: 'center', paddingTop:80, paddingBottom: 20}}>
+        <View style={{ alignItems: 'center', alignSelf: 'auto', marginBottom:40 }}>
           <IconSkilss/>
-          <Text style={{fontWeight: '500', fontSize: 24, lineHeight:32, marginTop:16, letterSpacing:1, marginBottom:24, textAlign: 'center'}}>{tr().t('skills')}</Text>
-        </View>
- 
-          <FlatList
-            contentContainerStyle={[Css.LayoutShadow, {flex:1,  justifyContent:'space-around',
-              flexDirection: 'row', flexWrap: 'wrap',padding: 40, marginHorizontal:40,marginVertical:2}]}
-            data={st.skills} 
-            keyExtractor={item=> item.title}
-            renderItem={({item, index}) =>  
-              <View 
-                style={{alignItems:'center',backgroundColor: '#E8DBDB',padding:12, borderRadius:8, marginHorizontal:24}}>
-                <Text style={{fontWeight: '400', fontSize: 18, lineHeight:19, letterSpacing:.7, textAlign: 'center'}}>{item.title}</Text>
-              </View>
-            } 
-          />  
-          
+          <Text style={{fontWeight: '500', fontSize: 24, lineHeight:32, marginTop:16, letterSpacing:1, textAlign: 'center'}}>{tr().t('skills')}</Text>
+        </View> 
 
-        <View style={{flex:1, alignItems: 'center', paddingTop:80, paddingBottom: 20}}>
+        <View style={[Css.LayoutShadow, {padding:40, flexWrap: 'wrap', flexDirection:'row', alignItems:'center', justifyContent:'center', marginBottom:80, marginHorizontal: 20 }]}>
+          {st.skills.map((item, index) => 
+            <View key={index}
+              style={{alignItems:'center',backgroundColor: '#E8DBDB',paddingHorizontal:16, paddingVertical:12, borderRadius:4, margin:10}}>
+              <Text style={{fontWeight: '400', fontSize: 18, lineHeight:19, letterSpacing:.7, textAlign: 'center'}}>{item.title}</Text>
+            </View>
+          )}
+        </View> 
+
+        <View style={{ alignItems: 'center', paddingBottom: 20}}>
           <IconPortopolio/>
           <Text style={{fontWeight: '500', fontSize: 24, lineHeight:32, marginTop:16, letterSpacing:1, marginBottom:24, textAlign: 'center'}}>{tr().t('Portofolio')}</Text>
         </View>
 
-        <FlatList
-          contentContainerStyle={{justifyContent: 'center', flexShrink:1,
-            flexDirection: 'row', flexWrap: 'wrap', }}
-          data={st.recent} 
-          keyExtractor={item=> item.name}
-          renderItem={({item, index}) =>  
+        <View style={{flexWrap: 'wrap', flexDirection:'row', alignItems:'center', justifyContent:'center', marginBottom:80, marginHorizontal:20 }}>
+          {st.recent.map((item, index) => 
             <View key={index} 
               style={[Css.LayoutShadow, {alignItems: 'center', 
               width: 320, height: 360, justifyContent: 'center', margin:8}]}>
@@ -166,44 +180,40 @@ class LearnSomething extends Component {
                 </TouchableOpacity>
                 }
                 {item.and == '' && item.ios =='' &&
-                <TouchableOpacity disabled style={{marginHorizontal: 6, borderColor: '#B42929',borderWidth:1, padding:8, borderRadius:4, opacity:.4}}>
+                <TouchableOpacity disabled style={{marginHorizontal: 6, borderColor: '#B42929',borderWidth:1, padding:8, borderRadius:4, opacity:.5}}>
                   <Text style={{fontSize: 14,  color:'#B42929'}}>{tr().t('develop')}</Text> 
                 </TouchableOpacity>
                 }
               </View>
             </View>
-          } 
-        /> 
+          )}
+        </View> 
 
+        <View style={{ alignItems: 'center',}}>
+          <Text style={{fontWeight: '400', fontSize: 14, opacity:.9, letterSpacing:.8, marginBottom:16}}>Social Media</Text> 
 
-
-        <View style={{ alignItems: 'center', alignSelf:'auto', flex:1 }}> 
-          <FlatList
-            contentContainerStyle={{  alignItems:'center',
-              flexDirection: 'row', flexWrap: 'wrap',paddingVertical: 12 }}
-            data={st.constac} 
-            keyExtractor={item=> item.name}
-            renderItem={({item, index}) =>  
+          <View style={{flexWrap: 'wrap', flexDirection:'row', }}>
+            {st.constac.map((item, index) => 
               <TouchableOpacity key={index} onPress={() => { 
-                  Linking.openURL(item.link); 
-                }} activeOpacity={.7}  
-                style={{alignItems:'center',borderColor: '#B42929', borderWidth:1, borderRadius:46, 
-                  marginVertical: 8, marginHorizontal:8, opacity:1, width: 46, height:46, justifyContent: 'center'}}>
-                <Feather name={item.icon} style={{fontSize: 16, fontWeight: '500', color:'#B42929'}}/> 
-              </TouchableOpacity>
-            } 
-          />  
-          
-          <View style={{flexDirection: 'row', opacity:.7}}>
+                    Linking.openURL(item.link); 
+                  }} activeOpacity={.7}  
+                  style={{alignItems:'center',borderColor: '#B42929', borderWidth:1, borderRadius:46, 
+                    marginVertical: 8, marginHorizontal:12, opacity:1, width: 46, height:46, justifyContent: 'center'}}>
+                  <Feather name={item.icon} style={{fontSize: 16, fontWeight: '500', color:'#B42929'}}/> 
+                </TouchableOpacity>
+            )}
+          </View>
+
+          <View style={{flexDirection: 'row', opacity:.7,  alignItems: 'center', marginTop: 30}}>
             <Text style={{fontWeight: '300', letterSpacing:1, fontSize:14}}>Handcrafted by me </Text> 
-            <FontAwesome5 name={'copyright'} size={14} color={'#B42929'}/>
-            <Text style={{fontWeight: '300', letterSpacing:2, fontSize:14}}> agusrahmadimido</Text> 
+            <FontAwesome5 name={'copyright'} style={{color: '#B42929', fontSize: 14, width: 16, height:16}}/>
+            <Text style={{fontWeight: '300', letterSpacing:2, fontSize:14, justifyContent: 'center'}}> agusrahmadimido</Text> 
           </View>
           <View style={{flexDirection: 'row',alignItems:'center', justifyContent: 'center', padding:12}}>
-            <Text style={{fontWeight: '500', letterSpacing:2, opacity:.7}}>Made me </Text> 
+            <Text style={{fontWeight: '600', letterSpacing:1, opacity:.7, fontSize: 12}}>Made me </Text> 
             <Image
               resizeMode={'contain'}
-              style={{width: 64,height: 48, alignItems:'center', marginLeft:6 }}
+              style={{width: 48,height: 22, alignItems:'center', marginLeft:6 }}
               source={require('../assets/expoicon.png')}
             />
           </View>
